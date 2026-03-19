154e2dd3111ded6da8ddd8066372d07e4927dfd4
Digvijay Singh <digvijaysingh8116@gmail.com>
2026-03-20 00:07:08 +0530

    1--> use chakra use version 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Chats from "./pages/ChatPage";
-import Homepage from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/chats" element={<Chats />}></Route>
       </Routes>
     </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Box, Container, Tabs, Text } from "@chakra-ui/react";
+import React from "react";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <Container maxW="xl">
+      <Box
+        display="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          Lets Chat
+        </Text>
+      </Box>
+      <Tabs.Root lazyMount unmountOnExit defaultValue="tab-1">
+        <Tabs.List>
+          <Tabs.Trigger value="tab-1">Tab 1</Tabs.Trigger>
+          <Tabs.Trigger value="tab-2">Tab 2</Tabs.Trigger>
+        </Tabs.List>
+        <Tabs.Content value="tab-1">
+          Tab 1: Content 
+        </Tabs.Content>
+        <Tabs.Content value="tab-2">
+          Tab 2: Content 
+        </Tabs.Content>
+      </Tabs.Root>
+      <Box></Box>
+    </Container>
+  );
+};
 
-export default Homepage
+export default Homepage;
