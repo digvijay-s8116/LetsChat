d6fca3122dc1605e753efcca2707d1c26ae6af7c
Digvijay Singh <digvijaysingh8116@gmail.com>
2026-03-20 11:27:40 +0530

    1--> created a basic ui for sign up

---
+++ frontend/src/components/aunthentication/Signup.jsx
@@ -0,0 +1,92 @@
+import { Input, VStack, Field, InputGroup, Button } from "@chakra-ui/react";
+import { useState } from "react";
+
+const Signup = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmpassword, setConfirmPassword] = useState();
+  const [pic, setPic] = useState();
+  const [show, setShow] = useState(false);
+  const [show2, setShow2] = useState(false);
+
+  function postDetails(pics) {}
+
+  function submitHandler() {}
+
+  return (
+    <VStack spacing="5px" color="black">
+      <Field.Root id="firstName" required>
+        <Field.Label>Name</Field.Label>
+        <Input
+          placeholder="Enter Your Name"
+          onChange={(e) => setName(e.target.value)}
+        ></Input>
+      </Field.Root>
+
+      <Field.Root id="" required>
+        <Field.Label>Email</Field.Label>
+        <Input
+          placeholder="Enter Your email"
+          onChange={(e) => setName(e.target.value)}
+        ></Input>
+      </Field.Root>
+
+      <Field.Root id="password" required>
+        <Field.Label>Password</Field.Label>
+        <InputGroup
+          endElement={
+            <Button size="sm" onClick={() => setShow(!show)}>
+              {show ? "hide" : "show"}
+            </Button>
+          }
+        >
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </InputGroup>
+      </Field.Root>
+
+      <Field.Root id="confirmPassword" required>
+        <Field.Label>Confirm Password</Field.Label>
+        <InputGroup
+          endElement={
+            <Button size="sm" onClick={() => setShow2(!show2)}>
+              {show2 ? "hide" : "show"}
+            </Button>
+          }
+        >
+          <Input
+            type={show ? "text" : "ConfirmPassword"}
+            placeholder="Enter Your ConfirmPassword"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </InputGroup>
+      </Field.Root>
+
+      <Field.Root>
+        <Field.Label>
+          <Input
+            type="file"
+            p={1.5}
+            accept="image/*"
+            onChange={(e) => postDetails(e.target.files[0])}
+          ></Input>
+        </Field.Label>
+      </Field.Root>
+
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Sign Up
+      </Button>
+    </VStack>
+  );
+};
+
+export default Signup;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import { Box, Container, Tabs, Text } from "@chakra-ui/react";
 import React from "react";
+import Signup from "../components/aunthentication/Signup";
+import Login from "../components/aunthentication/Login";
 
 const Homepage = () => {
   return (
@@ -18,19 +20,43 @@ const Homepage = () => {
           Lets Chat
         </Text>
       </Box>
-      <Tabs.Root lazyMount unmountOnExit defaultValue="tab-1">
-        <Tabs.List>
-          <Tabs.Trigger value="tab-1">Tab 1</Tabs.Trigger>
-          <Tabs.Trigger value="tab-2">Tab 2</Tabs.Trigger>
-        </Tabs.List>
-        <Tabs.Content value="tab-1">
-          Tab 1: Content 
-        </Tabs.Content>
-        <Tabs.Content value="tab-2">
-          Tab 2: Content 
-        </Tabs.Content>
-      </Tabs.Root>
-      <Box></Box>
+
+      <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="lg"
+        color="black"
+        borderWidth="1px"
+      >
+        <Tabs.Root lazyMount unmountOnExit defaultValue="tab-1" variant="plain">
+          <Tabs.List mb="1em" borderBottom="none" display="flex">
+            <Tabs.Trigger
+              value="tab-1"
+              borderRadius="full"
+              px={4}
+              py={2}
+              width="50%"
+              _selected={{ bg: "green", color: "white" }}
+            >
+              Login
+            </Tabs.Trigger>
+
+            <Tabs.Trigger
+              value="tab-2"
+              borderRadius="full"
+              px={4}
+              py={2}
+              width="50%"
+              _selected={{ bg: "green", color: "white" }}
+            >
+              Sign up
+            </Tabs.Trigger>
+          </Tabs.List>
+          <Tabs.Content value="tab-1">{<Login />}</Tabs.Content>
+          <Tabs.Content value="tab-2">{<Signup />}</Tabs.Content>
+        </Tabs.Root>
+      </Box>
     </Container>
   );
 };
