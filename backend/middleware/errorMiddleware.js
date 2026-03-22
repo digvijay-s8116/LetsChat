3c5e0e1ada65c154f62767e63eaea4856ee304cb
Digvijay Singh <digvijaysingh8116@gmail.com>
2026-03-22 20:47:11 +0530

    1--> create a middleware to check for any unknown url request and also check for any error in backend

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,16 @@
+module.exports = {
+  notFound(req, res, next) {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+  },
+
+  errorHandler(err, req, res, next) {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+    return res.status(statusCode).json({
+      responseMessage: err.message,
+      stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+  },
+};
--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,32 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDb = require("./config/connectDb");
 const userRoutes = require("./api/user/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
-dotenv.config();
-
-connectDb();
 const app = express();
 
-app.use(express.json());
-
+//  use this middleware to solve CORS error
 const cors = require("cors");
 app.use(cors());
+
+// call function for env configuration
+dotenv.config();
+
+// call this function to connect dataBase
+connectDb();
+
+// middleware to accept data in req
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("server is up and running ");
 });
 
-
 app.use("/api/user", userRoutes);
+
+// If not above route Found or have any error then check in the middle ware
+app.use(notFound); // this will handle any unknown URLs
+app.use(errorHandler); //  this will handle any error
 
 app.listen(5000, () => {
   console.log("Server is runnig on port number 5000");
